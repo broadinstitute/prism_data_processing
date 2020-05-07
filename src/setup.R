10fc4c181b0b83a38ae80724d0ba8f8e8a5776ac
aboghossian <aboghoss@broadinstitute.org>
2020-05-07 14:53:05 -0400

    rename install script

---
+++ src/setup.R
@@ -0,0 +1,19 @@
+# packages required for pipeline
+options(repos=structure(c(CRAN="http://cran.r-project.org")))
+
+install.packages(c("tidyverse",
+                   "useful",
+                   "magrittr",
+                   "scam",
+                   "dr4pl",
+                   "readr",
+                   "ranger",
+                   "glmnet",
+                   "sandwich",
+                   "BiocManager",
+                   "PRROC")
+                 )
+
+BiocManager::install()
+BiocManager::install(c("limma",
+                       "sva"))
